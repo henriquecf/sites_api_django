1ab548bf86d38ef30bb879fa2a096609d9b541e0
Ivan Morais <ivan.eng.controle@gmail.com>
2014-01-30 10:35:42 -0200

    Created test if slug is generated automatically

--- publication/publication/test_publication.py
+++ publication/publication/test_publication.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from django.core.urlresolvers import reverse
+from django.utils.text import slugify
 from rest_framework.test import APILiveServerTestCase
 from rest_framework import status
 
@@ -24,3 +25,34 @@ class CreatePublicationAPITestCase(APILiveServerTestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, data)
+
+    def test_post_blank_publication(self):
+        """
+        Ensure it does not accept the data when some mandatory fields are sent without data
+        """
+        url = reverse('publication-list')
+        data = {
+            'title': None,
+            'description': None,
+            'slug': None,
+            'publication_start_date': None,
+            'publication_end_date': None,
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_slug_is_generated_automatically(self):
+        """
+        Checks if the slug is generated automatically
+        """
+        url = reverse('publication-list')
+        data = {
+            'title': 'First publication',
+            'description': 'First description',
+            'slug': 'other-slug',
+            'publication_start_date': datetime(2014, 1, 29, 19, 10, 7),
+            'publication_end_date': None,
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['slug'], slugify(data['title']))
