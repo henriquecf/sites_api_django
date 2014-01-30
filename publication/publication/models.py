dd07b6433b821eb33972ee497a6ab5a744d72675
Ivan Morais <ivan.eng.controle@gmail.com>
2014-01-30 11:03:29 -0200

    Passed test if slug is generated automatically

--- publication/publication/models.py
+++ publication/publication/models.py
@@ -1,9 +1,16 @@
 from django.db import models
-
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.text import slugify
 
 class Publication(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField()
-    slug = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150)
     publication_start_date = models.DateTimeField()
     publication_end_date = models.DateTimeField(blank=True, null=True)
+
+
+@receiver(pre_save, sender=Publication)
+def slugify_title(sender, instance, *args, **kwargs):
+    instance.slug = slugify(instance.title)
