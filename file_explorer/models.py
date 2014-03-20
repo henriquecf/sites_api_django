2db9099cad6812e2acbd2d4f50d5f8d54dcb44bf
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2014-03-20 09:08:50 -0300

    Added todo about file post in file explorer

--- file_explorer/models.py
+++ file_explorer/models.py
@@ -4,6 +4,7 @@ from publication.models import Publication, Category
 
 
 class File(Publication):
+    # TODO Find a way to test post file. This field must not be blank
     file = models.FileField(upload_to='files', blank=True)
     categories = models.ManyToManyField(Category, null=True, blank=True, related_name='files')
 
