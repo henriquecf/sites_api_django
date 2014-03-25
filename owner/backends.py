4071941b3a2b98b36201d5cb2c693f0dae0fc5fe
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2014-03-25 17:10:26 -0300

    Created custom DjangoModelPermissions with read permission in GET method

--- owner/backends.py
+++ owner/backends.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
-from rest_framework import filters
+from rest_framework import filters, permissions
+
+custom_permissions_map = {
+    'GET': ['%(app_label)s.read_%(model_name)s'],
+    'OPTIONS': [],
+    'HEAD': [],
+    'POST': ['%(app_label)s.add_%(model_name)s'],
+    'PUT': ['%(app_label)s.change_%(model_name)s'],
+    'PATCH': ['%(app_label)s.change_%(model_name)s'],
+    'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+}
+
+
+class DjangoModelCustomPermissions(permissions.DjangoModelPermissions):
+    perms_map = custom_permissions_map
 
 
 class IsOwnerFilterBackend(filters.BaseFilterBackend):
