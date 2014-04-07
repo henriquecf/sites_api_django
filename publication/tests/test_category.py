cda2a0a44a2affe4b104ad56a2fcf64e7626b5c6
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2014-04-06 21:40:05 -0300

    Re implemented category tests #72 Done

--- publication/tests/test_category.py
+++ publication/tests/test_category.py
@@ -1,1 +1,79 @@
 # -*- coding: utf-8 -*-
+from copy import copy
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APILiveServerTestCase
+import routines, fixtures
+from resource import routines as resource_routines
+from publication import routines as publication_routines
+from news.models import News
+
+
+class CategoryAPITestCase(APILiveServerTestCase):
+    model = News
+
+    def setUp(self):
+        self.url = reverse('category-list')
+        self.data = {
+            'name': 'Category 1',
+            'model_name': 'uncategorized'
+        }
+        self.altered_data = {
+            'name': 'Category 1 altered',
+            'model_name': 'uncategorized'
+        }
+        fixtures.user_accountuser_account_token_fixture(self)
+        self.set_authorization_bearer()
+        self.first_object_response = self.client.post(self.url, self.data)
+
+    def set_authorization_bearer(self, token=None):
+        if not token:
+            token = self.owner_token
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer {0}'.format(token))
+
+    def test_api_basic_methods(self):
+        routines.test_api_basic_methods_routine(self)
+
+    def test_resource_permission(self):
+        routines.test_resource_permission_routine(self)
+
+    def test_admin_permission(self):
+        routines.test_admin_permission_routine(self)
+
+    def test_custom_object_permission(self):
+        routines.test_custom_object_permission_routine(self)
+
+    def test_resource_hyperlinked_fields(self):
+        fields = ['get_descendants']
+        resource_routines.test_resource_serializer_hyperlinked_fields_routine(self, fields)
+
+    def test_model_has_custom_permission(self):
+        routines.test_model_has_custom_permission_routine(self)
+
+    def test_if_creates_with_parent(self):
+        response = self.client.post(self.url, self.data)
+        children_data = copy(self.data)
+        children_data.update({'parent': response.data['url'], 'name': 'Category 2'})
+        response2 = self.client.post(self.url, children_data)
+        self.assertEqual(response2.data['parent'], response.data['url'])
+
+    def test_get_descendants(self):
+        response = self.client.post(self.url, self.data)
+        children_data = copy(self.data)
+        children_data.update({'parent': response.data['url'], 'name': 'Category 2'})
+        response2 = self.client.post(self.url, children_data)
+        response3 = self.client.get(response.data['url'])
+        self.assertIn('get_descendants', response3.data)
+        get_descendants_url = response3.data['get_descendants']
+        response4 = self.client.get(get_descendants_url)
+        self.assertEqual(response4.data['descendants'][0], response2.data)
+
+    def test_is_leaf_node(self):
+        response = self.client.post(self.url, self.data)
+        self.assertIn('is_leaf_node', response.data)
+        self.assertTrue(response.data['is_leaf_node'])
+        children_data = copy(self.data)
+        children_data.update({'parent': response.data['url'], 'name': 'Category 2'})
+        self.client.post(self.url, children_data)
+        response2 = self.client.get(response.data['url'])
+        self.assertFalse(response2.data['is_leaf_node'])
