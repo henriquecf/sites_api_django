4c1f2b73123f9b243d4fb0dd7de671d01c5fcd11
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2014-04-09 12:12:48 -0300

    Setting a default value for file field to not leave it blank. Still need to solve this in order not to have a default value #27

--- file_explorer/serializers.py
+++ file_explorer/serializers.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+from rest_framework import serializers
 from publication.serializers import PublicationSerializer
-from .models import File
+from file_explorer.models import File
 
 
 class FileSerializer(PublicationSerializer):
+    file = serializers.FileField(blank=True)
 
     class Meta(PublicationSerializer.Meta):
         model = File
