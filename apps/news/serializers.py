852fdae204776f0c1e00c1fdc518facffd0acb01
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2014-04-11 16:46:28 -0300

    Corrected error in news serializer related to image field

--- apps/news/serializers.py
+++ apps/news/serializers.py
@@ -1,11 +1,8 @@
-from rest_framework import serializers
-
 from apps.publication.serializers import PublicationSerializer
 from apps.news.models import News
 
 
 class NewsSerializer(PublicationSerializer):
-    image = serializers.ImageField(source='news.image', required=False)
 
     class Meta(PublicationSerializer.Meta):
         model = News
