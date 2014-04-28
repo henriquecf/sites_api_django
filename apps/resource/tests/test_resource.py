0bee300edf4a12a29b6f0912573503521e67cfb1
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2014-04-28 14:19:53 -0300

    Finished tests for ResourceViewSet methods in resource #117

--- apps/resource/tests/test_resource.py
+++ apps/resource/tests/test_resource.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 from django.test.testcases import TestCase
 from django.http.request import HttpRequest
+
 from apps.resource.serializers import ResourceSerializer
-from apps.resource.models import Site, Resource, ContribSite
+from apps.resource.models import Site, Resource, ContribSite, AuthUser, User
+from apps.resource.views import ResourceViewSet
 from test_fixtures import user_accountuser_account_permissions_token_fixture
 
 
 class ResourceTestCase(TestCase):
     model = Resource
 
-    def setUp(self):
-        user_accountuser_account_permissions_token_fixture(self)
-
     def test_sites_filter_get_fields_serializer(self):
+        user_accountuser_account_permissions_token_fixture(self)
         request = HttpRequest()
         request.user = self.owner
         site = ContribSite.objects.first()
-        accountsite = Site.objects.create(owner=self.owner, site=site, author=self.owner)
+        Site.objects.create(owner=self.owner, site=site, author=self.owner)
         possible_sites = ResourceSerializer(context={'request': request}).get_fields()['sites'].queryset
         self.assertIn(site, possible_sites)
 
         other_site = ContribSite.objects.create()
-        other_user_accountsite = Site.objects.create(owner=self.second_owner, site=other_site,
-                                                     author=self.second_owner)
+        Site.objects.create(owner=self.second_owner, site=other_site,
+                            author=self.second_owner)
         possible_sites = ResourceSerializer(context={'request': request}).get_fields()['sites'].queryset
         self.assertIn(site, possible_sites)
         self.assertNotIn(other_site, possible_sites)
@@ -31,3 +31,21 @@ class ResourceTestCase(TestCase):
         possible_sites = ResourceSerializer(context={'request': request}).get_fields()['sites'].queryset
         self.assertNotIn(site, possible_sites)
         self.assertIn(other_site, possible_sites)
+
+    def test_pre_save_post_save_methods(self):
+        user = AuthUser.objects.create_user(username='user', password='123')
+        User.objects.create(owner=user, author=user, user=user)
+        request = HttpRequest()
+        request.user = user
+        viewset = ResourceViewSet(request=request)
+        resource = Resource()
+        viewset.pre_save(resource)
+        self.assertEqual(user, resource.owner)
+        self.assertEqual(user, resource.author)
+
+        resource.save()
+        request.META['HTTP_HOST'] = 'testserver'
+
+        viewset.post_save(resource)
+        site = ContribSite.objects.get(domain='testserver')
+        self.assertIn(site, resource.sites.all())
