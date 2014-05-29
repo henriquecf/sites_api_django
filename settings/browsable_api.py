9191e0adf73cc13b16ac28e126dec8f3291d2496
Ivan Morais <ivan.eng.controle@gmail.com>
2014-05-29 14:42:13 -0300

    Added a personalized version of django rest framework to show field options in API Options

--- settings/browsable_api.py
+++ settings/browsable_api.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from settings.dev import *
 
+ALLOWED_HOSTS = ['http://localhost:63342/']
+
 REST_FRAMEWORK = {
     'PAGINATE_BY': 10,
     'PAGINATE_BY_PARAM': 'page_size',
@@ -12,6 +14,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.UnicodeJSONRenderer',
+        'rest_framework.renderers.HTMLFormRenderer',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
